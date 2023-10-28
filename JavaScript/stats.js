5836eccec2a686636aa42fc6689045d2aeb4e2b9
Spencer-Garnder <spencegard@gmail.com>
2023-10-27 18:38:41 -0600

    update data display

--- JavaScript/stats.js
+++ JavaScript/stats.js
@@ -1,26 +1,24 @@
+function updateUser() {
+    const user = document.getElementById("user");
+    user.innerText = localStorage.getItem("username") + "'s Stats";
+}
+
 function updateStats() {
     const plays = document.getElementById("plays");
     const wins = document.getElementById("wins");
-
-    plays.textContent = localStorage.getItem("plays");
-    wins.textContent = localStorage.getItem("wins");
+    plays.innerText = localStorage.getItem("plays");
+    wins.innerText = localStorage.getItem("wins");
 }
 
 function updateScores() {
     const score1 = document.getElementById("score1");
-    const date1 = document.getElementById("date1");
     const score2 = document.getElementById("score2");
-    const date2 = document.getElementById("date2");
-    const score3 = document.getElementById("score3");
-    const date3 = document.getElementById("date3");
-
-    score1.textContent = localStorage.getItem("score1");
-    date1.textContent = localStorage.getItem("date1");
-    score2.textContent = localStorage.getItem("score2");
-    date2.textContent = localStorage.getItem("date2");
-    score3.textContent = localStorage.getItem("score3");
-    date3.textContent = localStorage.getItem("date3");
+    const score3 = document.getElementById("score3");    
+    score1.innerText = localStorage.getItem("score1");
+    score2.innerText = localStorage.getItem("score2");
+    score3.innerText = localStorage.getItem("score3");
 };
 
+updateUser();
 updateStats();
 updateScores();
