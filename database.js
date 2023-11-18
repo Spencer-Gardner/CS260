b1c5a8f90119ab04075ae88769e467d3a0aee929
Spencer-Garnder <spencegard@gmail.com>
2023-11-17 22:28:11 -0700

    mongodb

--- database.js
+++ database.js
@@ -1,41 +1,31 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-async function main() {
-  // Connect to the database cluster
-  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-  const client = new MongoClient(url);
-  const db = client.db('rental');
-  const collection = db.collection('house');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const statsCollection = db.collection('stats');
 
-  // Test that you can connect to the database
-  (async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-  })().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-  });
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
 
-  // Insert a document
-  const house = {
-    name: 'Beachfront views',
-    summary: 'From your bedroom to the beach, no shoes required',
-    property_type: 'Condo',
-    beds: 1,
-  };
-  await collection.insertOne(house);
-
-  // Query the documents
-  const query = { property_type: 'Condo', beds: { $lt: 2 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-
-  const cursor = collection.find(query, options);
-  const rentals = await cursor.toArray();
-  rentals.forEach((i) => console.log(i));
+async function updateStats(stats) {
+  const query = { _id: 'stats' };
+  const update = { $set: stats };
+  const response = await statsCollection.updateOne(query, update);
+  return response;
 }
 
-main().catch(console.error);
+async function getStats() {
+  const query = { _id: 'stats' };
+  const response = await statsCollection.findOne(query);
+  return response;
+}
+
+module.exports = { updateStats, getStats };
