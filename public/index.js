1a0d642bd1417fa4656eeb1ce80ac800ac715295
Spencer-Garnder <spencegard@gmail.com>
2023-11-18 19:48:56 -0700

    add login functions

--- public/index.js
+++ public/index.js
@@ -1,5 +1,75 @@
-function login() {
-    const username = document.querySelector("#username");
-    localStorage.setItem("username", username.value);
-    window.location.href = "play.html";
+'use strict';
+
+// LOGIN DISPLAY...
+(async () => {
+  const username = localStorage.getItem('username');
+  if (username) {
+    document.getElementById('user').innerText = username;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
   }
+})();
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
+
+
+// PRIMARY FUNCTIONS...
+async function login() {
+  verify(`/api/auth/login`);
+}
+
+async function create() {
+  verify(`/api/auth/create`);
+}
+
+async function verify(endpoint) {
+  const username = document.querySelector('#username')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ username: username, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('username', username);
+    window.location.href = 'play.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+
+
+// SECONDARY FUNCTIONS...
+function play() {
+  window.location.href = 'play.html';
+}
+
+function logout() {
+  localStorage.removeItem('username');
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(username) {
+  const response = await fetch(`/api/user/${username}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+  return null;
+}
