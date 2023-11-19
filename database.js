6c60a0a7f6add2be12f3cb2cf229c1620b5154c7
Spencer-Garnder <spencegard@gmail.com>
2023-11-18 19:48:29 -0700

    add user database

--- database.js
+++ database.js
@@ -1,12 +1,17 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
+const userCollection = db.collection('user');
 const statsCollection = db.collection('stats');
 
-// This will asynchronously test the connection and exit the process if it fails
+
+// TEST CONNECTION...
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
@@ -15,17 +20,54 @@ const statsCollection = db.collection('stats');
   process.exit(1);
 });
 
-async function updateStats(stats) {
-  const query = { _id: 'stats' };
-  const update = { $set: stats };
+
+// USER FUNCTIONS...
+async function createUser(username, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    username: username,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+  const stats = {
+    username: username,
+    plays: 0,
+    wins: 0,
+    scores: [0,0,0]
+  }
+  await statsCollection.insertOne(stats);
+  return user;
+}
+
+function getUser(username) {
+  return userCollection.findOne({ username: username });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+// STATS FUNCTIONS...
+async function updateStats(username, stats) {
+  const query = { username: username };
+  const update = {
+    $set: {
+      plays: stats.plays,
+      wins: stats.wins,
+      scores: stats.scores
+    }
+  }
   const response = await statsCollection.updateOne(query, update);
   return response;
 }
 
-async function getStats() {
-  const query = { _id: 'stats' };
+async function getStats(username) {
+  const query = { username: username };
   const response = await statsCollection.findOne(query);
   return response;
 }
 
-module.exports = { updateStats, getStats };
+
+// EXPORT FUNCTIONS...
+module.exports = { createUser, getUser, getUserByToken, updateStats, getStats };
