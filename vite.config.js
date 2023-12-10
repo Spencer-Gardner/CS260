49d3e72c6ec358dc99bf3d438020b9a5a537a09d
Spencer-Garnder <spencegard@gmail.com>
2023-12-09 20:11:19 -0700

    create vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       '/api': 'http://localhost:4000',
       '/ws': {
         target: 'ws://localhost:4000',
-        ws: true,
+        ws: true
       },
     },
   },
