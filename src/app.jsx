797b33fc619358dd3972cae9d12a48805474f29a
Spencer-Garnder <spencegard@gmail.com>
2023-12-13 17:03:33 -0700

    update routes and authentication

--- src/app.jsx
+++ src/app.jsx
@@ -4,45 +4,63 @@ import { Login } from './home/login';
 import { Play } from './play/play';
 import { Stats } from './stats/stats';
 import { About } from './about/about';
+import { AuthState } from './home/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+
     return (
         <BrowserRouter>
-    <div className='body bg-dark text-light'>
-        <header className='container-fluid bg-black text-white'>
-            <nav className='navbar fixed-top navbar-dark sticky-top'>
-                <div className='navbar-brand'>
-                    {/* <img src='https://www.activeliving.ie/content/uploads/2020/04/placeholder-logo-2.png' alt='logo' width='50px'> </img> */}
-                    Wordlock
-                </div>  
-                <menu className='navbar-nav'>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to=''>Home</NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='play'>Play</NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='stats'>Stats</NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='about'>About</NavLink>
-                    </li>
-                </menu>
-            </nav>
-        </header>
+            <div className='body'>
+                <header className='container-fluid bg-black text-white'>
+                    <nav className='navbar navbar-dark fixed-top sticky-top'>
+                        <div className='navbar-brand'>
+                            <img src='https://www.activeliving.ie/content/uploads/2020/04/placeholder-logo-2.png' alt='logo' width='50px'></img>
+                            Wordlock
+                        </div>
+                        <menu className='navbar-nav'>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to=''>Home</NavLink>
+                            </li>
+                            {authState === AuthState.Authenticated && (
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' to='play'>Play</NavLink>
+                                </li>
+                            )}
+                            {authState === AuthState.Authenticated && (
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' to='stats'>Stats</NavLink>
+                                </li>
+                            )}
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='about'>About</NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
 
         <Routes>
-            <Route path='/' element={<Login />} exact />
+            <Route path='/' element={
+                <Login
+                    userName={userName}
+                    authState={authState}
+                    onAuthChange={(userName, authState) => {
+                        setAuthState(authState);
+                        setUserName(userName);
+                    }}
+            />
+            } exact />
             <Route path='/play' element={<Play />} />
             <Route path='/stats' element={<Stats />} />
             <Route path='/about' element={<About />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
 
-        <footer className='bg-black p-2 text-white-50'>
+        <footer className='bg-black text-white-50 p-2 fixed-bottom'>
             <div className='container-fluid'>
                 <span className='text-reset'>Spencer Gardner</span>
                 <NavLink className='text-reset' href='https://github.com/Spencer-Gardner/CS_260'>GitHub</NavLink>
