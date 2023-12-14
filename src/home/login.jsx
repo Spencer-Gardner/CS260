5c43b8d6b98478700e374a879ccfc8d418c4b14d
Spencer-Garnder <spencegard@gmail.com>
2023-12-13 17:07:48 -0700

    implement login logic

--- src/home/login.jsx
+++ src/home/login.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export function Login() {
-    return (
-      <main className='container-fluid bg-secondary text-center'>
-        <div>login displayed here</div>
-      </main>
-    );
-  }
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
+  return (
+    <main className="container-fluid justify-content-start align-items-center pt-5">
+      <div>
+        {authState !== AuthState.Unknown && <h1 className="text-center pt-4 pb-4">Welcome!</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+    </main>    
+  );
+}
